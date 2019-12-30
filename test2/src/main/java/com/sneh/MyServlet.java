9b18e00123f1e66cd7ad12e443378631e26d3c22
sneh3195 <snehjainnsk@gmail.com>
2019-12-30 18:32:17 +0530

    starter project

---
+++ test2/src/main/java/com/sneh/MyServlet.java
@@ -0,0 +1,18 @@
+package com.sneh;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public MyServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
